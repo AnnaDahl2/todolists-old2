e6ba41f079de99c4e0879a3d3916f254ffdfae30
annadahl2 <anna@riktigklode.no>
2016-02-14 13:54:44 +0000

    Rq11 passed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,20 @@
 class Profile < ActiveRecord::Base
-  belongs_to :user
+    belongs_to :user
+  
+    validates :gender, inclusion: { in: %w(female male), message: "%{value} is not a valid gender" }
+    
+    validate :both_names_null
+    validate :male_not_sue
+    
+    def both_names_null 
+        if (first_name == nil) and (last_name == nil)
+            errors.add(:profile, "Both names can't be missing")
+        end
+    end
+    
+    def male_not_sue
+        if (gender == "male") and (first_name == "Sue")
+            errors.add(:profile, "Sue is not a male name")
+        end
+    end
 end
